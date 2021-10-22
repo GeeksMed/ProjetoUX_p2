dbb246a0b4cf9f980e8048e12761a8641ea1045c
Unknown <silva.germain@gmail.com>
2021-10-22 11:44:18 -0300

    Criar pagina login
    Iniciar com página login

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template, request, url_for
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
 
+@app.route("/login/", methods=['POST'])
+def get_nome():
+    nomeLogin = request.form.get('textarea')
+    return render_template("home.html", nome = nomeLogin)
