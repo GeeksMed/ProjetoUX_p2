1cb94a9df561ddd620105b294f1d78d024b8997a
Unknown <silva.germain@gmail.com>
2021-10-22 23:16:14 -0300

    Inicio Página de Login com GRID e FLEXBOX
    Iniciei a configuração do CSS e do HTML para realizar as funções dos botões e deixar o layout como o enviado no trabalho da P1.

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -1,11 +1,23 @@
 from application import app
 from flask import Flask, render_template, request, url_for
 
+
 @app.route('/')
 def index():
     return render_template("login.html")
+
 
 @app.route("/login/", methods=['POST'])
 def get_nome():
     nomeLogin = request.form.get('textarea')
     return render_template("home.html", nome = nomeLogin)
+
+
+@app.route("/lostpassword/") #, methods=["POST"])
+def esqueceu():
+    return render_template("forget_password.html")
+
+
+@app.route("/newaccount/") #, methods=["POST"])
+def nova_conta():
+    return render_template("novaconta.html")
