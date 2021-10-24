985088b2cab9dd1cdf66317abae97a59aaa45879
Unknown <silva.germain@gmail.com>
2021-10-24 16:51:37 -0300

    Telas Inicias
    Arrumei back e terminei todas as telas antes do Index (Login e funcionalidades).

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -7,25 +7,34 @@ def index():
     return render_template("login.html")
 
 
-@app.route("/login/", methods=['POST'])
-def get_nome():
-    nomeLogin = request.form.get('textarea')
-    if nomeLogin == None:
-        nomeLogin = "Welcome Tester"
-    return render_template("index.html", nome = nomeLogin)
-
-
-@app.route("/login/")
-def login_gmail():
-    nomeLogin = "Login Gmail"
-    return render_template("index.html", nome=nomeLogin)
-
-
-@app.route("/lostpassword/") #, methods=["POST"])
+@app.route("/lostpassword") #, methods=["POST"])
 def esqueceu():
     return render_template("forget_password.html")
 
 
-@app.route("/newaccount/") #, methods=["POST"])
+@app.route("/recuperalogin", methods=["POST"])
+def recuperar():
+    nomeLogin = request.form.get('textarea')
+    if nomeLogin == None:
+        nomeLogin = "Login Recuperado"
+    return render_template("recupera.html", nome=nomeLogin)
+
+
+@app.route("/newaccount") #, methods=["POST"])
 def nova_conta():
     return render_template("novaconta.html")
+
+
+@app.route("/login", methods=['POST'])
+def get_nome():
+    nomeLogin = request.form.get('textarea')
+    print("nomeLogin = ", nomeLogin)
+    if nomeLogin == None or nomeLogin == "":
+        nomeLogin = "Welcome Tester"
+    return render_template("index.html", nome=nomeLogin)
+
+
+@app.route("/login")
+def login_gmail():
+    nomeLogin = "Login Gmail"
+    return render_template("index.html", nome=nomeLogin)
